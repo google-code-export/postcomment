0d790150ad2f650f7aa092c223e87fa76f3d73fc
libiun <libiun@0dc055d2-28ac-11de-897e-ef2ff8275b85>
2009-04-14 04:37:11 +0000

    modify Main.java

--- trunk/JavaBasic/src/javabasic/Main.java
+++ trunk/JavaBasic/src/javabasic/Main.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package javabasic;
 
 /**
@@ -16,6 +15,6 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("change happens");
     }
-
 }
