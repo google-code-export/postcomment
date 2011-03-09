7dc272e7e41ede685d51fa36cd938a2f6885c544
libiun <libiun@0dc055d2-28ac-11de-897e-ef2ff8275b85>
2011-03-09 13:09:54 +0000

    adfadsf

---
+++ trunk/testcodegooglesvn/src/testcodegooglesvn/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testcodegooglesvn;
+import java.awt.*;
+
+/**
+ *
+ * @author libiun
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame f=new Frame();
+        f.setTitle("hello you");
+        f.setBounds(100,100,100,100);
+        f.setVisible(true);
+
+    }
+
+}
